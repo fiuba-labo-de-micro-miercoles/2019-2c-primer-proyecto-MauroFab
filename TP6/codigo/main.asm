1daf060e2ceb4b6060d17d040e1ab3b3bb360728
MauroFab <maurotoscano2@gmail.com>
2020-08-16 21:34:13 -0300

    Agregado anti rebote

--- TP6/codigo/main.asm
+++ TP6/codigo/main.asm
@@ -8,13 +8,19 @@
 .EQU PUERTO_ENTRADA= PIND
 .EQU BIT_BOTON_0 = 0
 .EQU BIT_BOTON_1 = 1
+.macro	setStack
+	ldi	r16,LOW(RAMEND)
+	out	SPL,r16
+	ldi	r16,HIGH(RAMEND)
+	out	SPH,r16
+.endmacro
 .include "m2560def.inc"
 .org 0x0000
 	jmp MAIN
 .org INT0addr
 	jmp isr_int0
 .org INT1addr
-	jmp isr_int1
+	jmp isr_int1                 
 .org 0x0028 // Timer1 Overflow Handler
 	jmp timer_int
 
@@ -35,6 +41,9 @@ MAIN:
 
 	ldi r28, 1 // flag led prendido
 	sbi PUERTO_SALIDA, BIT_LED_0
+
+	setStack
+
 	bset 7; habilito interrupciones en status register 
 loop:
 	jmp loop
@@ -49,10 +58,10 @@ apagar:
 	ldi r28, 0
 	cbi PUERTO_SALIDA, BIT_LED_0
 	reti
-	
 
 isr_int1:
 isr_int0:
+	call delay_anti_rebote
 	in r20, PUERTO_ENTRADA
 	// Esta mascara no deberia ser necesaria porque no hay nada mas conectado
 	// la uso por claridad que me interesan esos bits
@@ -69,22 +78,56 @@ isr_int0:
 	breq pre_escaler_256
 	cpi r20, (1 << BIT_BOTON_0 | 1 << BIT_BOTON_1)
 	breq stop_timer
+fin_isr:
 	reti
 
 stop_timer:
-	LDI r16, (0 << CS02 | 0 << CS01 | 0 << CS00)    ;SET TIMER PRESCALER
+	LDI r16, (0 << CS02 | 0 << CS01 | 0 << CS00)
 	sts TCCR1B,r16
 	sbi PUERTO_SALIDA, BIT_LED_0
-	reti
+	rjmp fin_isr
+
 pre_escaler_64:
 	LDI r16, (0 << CS02 | 1 << CS01 | 1 << CS00)    ;SET TIMER PRESCALER
 	sts TCCR1B,r16
-	reti
+	rjmp fin_isr
+
 pre_escaler_256:
 	LDI r16, (1 << CS02 | 0 << CS01 | 0 << CS00)    ;SET TIMER PRESCALER
 	sts TCCR1B,r16
-	reti
+	rjmp fin_isr
+
 pre_escaler_1024:
 	LDI r16, (1 << CS02 | 0 << CS01 | 1 << CS00)    ;SET TIMER PRESCALER
 	sts TCCR1B,r16
-	reti
+	rjmp fin_isr
+
+delay_anti_rebote:
+	//Configuro T3 con un pre escaler para usar de espera y evitar captar rebotes
+	//T3 es un timer de 16 bits
+	//Voy a usarlo manualmente, sin interrupciones, asi que no configuro nada más
+	
+	//Pre escaler en 1024, lo que da unos 4.2s de delay aproximadamente
+	//Esto es para mostrarlo andando y testear
+	ldi r16, (1 << CS02 | 0 << CS01 | 1 << CS00)
+	//En una situación mas realista
+	//el pre escaler de 8 que da unos 32 ms de delay sería mas adecuado
+	//También se podria usar el timer de 256 bits y un pre escaler de 1024
+	//Para usar el pre escaler de 8, descomentar la siguiente linea
+	//ldi r16, (0 << CS02 | 1 << CS01 | 0 << CS00 )
+	//Por si se uso antes, limpio el flag del overflow
+	sts TCCR3B,r16
+	sbi TIFR3, TOV0
+	//Pongo el contador en 0, si no los tiempos varían
+	ldi r16, 0
+	sts TCNT3l, r16
+	sts TCNT3h, r16
+
+esperar_timer:
+	in   r16,TIFR3
+	andi r16, (1 << TOV0) ;overflow flag
+	cpi r16, 1
+	brne esperar_timer
+	ret
+
+	
